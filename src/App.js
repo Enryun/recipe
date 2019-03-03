162af7cc73b381ccdb993ebc292f0761d09b7aab
Duong Thang <duongthang@Duongs-iMac.net.fpt>
2019-03-04 04:10:11 +0700

    first project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {recipes} from './tempList';
+import RecipeList from './components/RecipeList';
+import RecipeDetail from './components/RecipeDetail';
 
 class App extends Component {
+
+  state = {
+      recipes: recipes,
+      url: 'https://www.food2fork.com/api/search?key=3f1d40996d23dc88f110b264b29fc73e',
+      base_url: 'https://www.food2fork.com/api/search?key=3f1d40996d23dc88f110b264b29fc73e',
+      detail_id: 35338,
+      pageIndex: 1,
+      search: '',
+      query: '&q=',
+      error: ''
+    };
+
+  async getRecipes () {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      console.log(jsonData);
+      if (jsonData.recipes.length === 0) {
+        this.setState(() => {
+          return {error: 'Sorry, there is no Recipe!'}
+        })
+      } else {
+        this.setState(() => {
+          return {recipes: jsonData.recipes}
+        });
+      }
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getRecipes()
+  }
+
+  displayPage = index => {
+    switch (index) {
+      default:
+      case 1: 
+        return <RecipeList recipes={this.state.recipes}
+                           handleDetails={this.handleDetails}
+                           value={this.state.search}
+                           handleChange={this.handleChange}
+                           handleSubmit={this.handleSubmit}
+                           error={this.state.error}
+        />;
+      case 0:
+        return  <RecipeDetail id={this.state.detail_id}
+                              handleIndex={this.handleIndex}
+        />;
+    }
+  }
+
+  handleIndex = index => {
+    this.setState({
+      pageIndex: index
+    })
+  }
+
+  handleDetails = (index, id) => {
+    this.setState({
+      pageIndex: index,
+      detail_id: id
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      search:e.target.value
+    },
+    () => {
+      console.log(this.state.search)
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const{base_url, query, search} = this.state;
+
+    this.setState(() => {
+      return {url:`${base_url}${query}${search}`, search: ""}
+    }, () => {
+      this.getRecipes();
+    })
+  }
+
   render() {
+    // console.log(this.state.recipes);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        {this.displayPage(this.state.pageIndex)}
+       
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/RecipeDetail.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import {recipe} from '../tempDetails';
+
+export default class RecipeDetail extends Component {
+
+
+  //First Way
+  // constructor (props) {
+  //     super(props)
+
+  //     this.state = {
+  //         recipe:recipe,
+  //         url:`https://www.food2fork.com/api/get?key=3f1d40996d23dc88f110b264b29fc73e&rId=${this.props.id}
+  //         `
+  //     }
+  // }
+  
+  // async componentDidMount() {
+  //   try {
+  //         const data = await fetch(this.state.url);
+  //         const jsonData = await data.json();
+      
+  //         this.setState({
+  //           recipe: jsonData.recipe
+  //         });
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  // }
+
+  // Second Way
+
+  state = {
+    recipe:recipe
+  }
+
+  async componentDidMount() {
+    const id = this.props.id;
+    const url = `https://www.food2fork.com/api/get?key=3f1d40996d23dc88f110b264b29fc73e&rId=${id}`;
+    try {
+              const data = await fetch(url);
+              const jsonData = await data.json();
+          
+              this.setState({
+                recipe: jsonData.recipe
+              });
+            } catch (error) {
+              console.log(error);
+            }
+  }
+
+  render() {
+
+    const{image_url, 
+      publisher, 
+      publisher_url, 
+      source_url,
+      title,
+      ingredients
+    } = this.state.recipe;
+
+    const {handleIndex} = this.props;
+    
+    return (
+        <React.Fragment>
+           <div className='container'>
+              <div className='row'>
+              {/* First column */}
+                <div className='col-10 mx-auto col-md-6 my-3'>
+                  <button type='button' 
+                          onClick={() => handleIndex(1)}
+                          className='btn btn-warning mb-5 text-capitalize'>
+                    back to recipe
+                  </button>
+                  <img src={image_url} className='d-block w-100' alt='recipe'/>
+                </div>
+                {/* Second column for Details */}
+                <div className='col-10 mx-auto col-md-6 my-3'>
+                  <h6 className='text-uppercase'>{title}</h6>
+                  <h6 className='text-warning text-capitalize text-slanted'>provided by {publisher}</h6>
+                  <a href={publisher_url}
+                    target='_blank'
+                    className='btn btn-primary mt-2 text-capitalize'
+                    rel="noopener noreferrer">
+                    publisher webpage</a>
+                  <a href={source_url}
+                    target='_blank'
+                    className='btn btn-success mx-3 mt-2 text-capitalize'
+                    rel="noopener noreferrer">
+                    recipe url</a>
+                  <ul className='list-group mt-4'>
+                    <h2 className='mt-3 mb-4'>Ingredients</h2>
+                    {ingredients.map((item, index) => {
+                      return (
+                        <li key={index} className='list-group-item text-slanted'>{item}</li>
+                      )
+                    })}
+                  </ul>
+                </div>
+              </div>
+           </div>
+        </React.Fragment>
+    )
+  }
+}
